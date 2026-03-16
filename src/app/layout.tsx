3ab9e77bf52c470605c3616d658ecf80217983ec
lubanrahat <lubanrahat.dev@gmail.com>
2026-03-16 20:47:59 +0600

    feat: add new dependencies and update layout and page components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,11 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { Inter } from "next/font/google";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const inter = Inter({
+  subsets: ['latin'], // required
+  variable: '--font-inter', // optional if you want to use CSS variable
 });
 
 export const metadata: Metadata = {
@@ -23,12 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <h1 className="text-xl text-fuchsia-400">CodeArena</h1>
+      <Button className="text-xl cursor-pointer bg-green-400 text-black py-5 px-10">Run</Button>
     </div>
   );
 }
